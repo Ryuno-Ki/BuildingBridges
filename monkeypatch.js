53411d0db1fd7490428f38ce2526cf4e614ac3f4
Andre Jaenisch <andre.jaenisch@openmailbox.org>
2014-05-16 15:52:52 +0200

    Adding Matrix object.

--- monkeypatch.js
+++ monkeypatch.js
@@ -1,3 +1,53 @@
+// Example call: 
+// var m = new Matrix([ [1,1,1,1],
+//                      [2,4,8,16],
+//                      [3,9,27,81],
+//                      [4,16,64,256],
+//                      [5,25,125,625] ]);
+function Matrix(ary) {
+    this.mtx    = ary
+    this.height = ary.length;
+    this.width  = ary[0].length;
+}
+ 
+Matrix.prototype.toString = function() {
+    var s = []
+    for (var i = 0; i < this.mtx.length; i++) 
+        s.push( this.mtx[i].join(",") );
+    return s.join("\n");
+}
+ 
+// returns a new matrix
+Matrix.prototype.transpose = function() {
+    var transposed = [];
+    for (var i = 0; i < this.width; i++) {
+        transposed[i] = [];
+        for (var j = 0; j < this.height; j++) {
+            transposed[i][j] = this.mtx[j][i];
+        }
+    }
+    return new Matrix(transposed);
+}
+
+Matrix.prototype.mult = function(other) {
+    if (this.width != other.height) {
+        throw "error: incompatible sizes";
+    }
+ 
+    var result = [];
+    for (var i = 0; i < this.height; i++) {
+        result[i] = [];
+        for (var j = 0; j < other.width; j++) {
+            var sum = 0;
+            for (var k = 0; k < this.width; k++) {
+                sum += this.mtx[i][k] * other.mtx[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return new Matrix(result); 
+}
+ 
 // Monkeypatch Math.min for accepting arrays
 var standardMin = Math.min;
 Math.min = function() {
@@ -21,3 +71,5 @@ function handleFileSelection() {
     waitForTextReadComplete(reader);
     reader.readAsText(file);
 };
+
+// http://rosettacode.org/wiki/Matrix_multiplication#JavaScript
