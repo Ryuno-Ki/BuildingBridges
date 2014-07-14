a5aa3e72ab9ff304e5c90ede20761f3d44262dda
Andre Jaenisch <AndreJaenisch@googlemail.com>
2014-07-14 14:51:09 +0200

    Removed demo code. Fixed insertIntoMatrix. Prepared solveLes().

--- js/les.js
+++ js/les.js
@@ -1,16 +1,3 @@
-/*
-var m = new Matrix([ [1,2],
-					 [3,3] ]);
-var part = m.lr();
-var r = part.r;
-var l = part.l;
-var a = [4,0];
-var x = [1,0];
-y = l.forwardSubstitution(x);
-x = r.backwardSubstitution(y);
-//console.log(m.mult(x).toString());
-*/
-
 function buildLes() {
 	// E   … Equationmatrix, E € IR^(16x18)
 	// f   … force, f € IR^18, f_k > 0 => Zugkraft, f_k < 0 => Druckkraft
@@ -25,43 +12,69 @@ function buildLes() {
 	// p = A*x with A = n*E*L^(-1)*E+
 	// f_k = n*d_k/l_k
 
-	// For every vector, multiply with -1 and determine cosine and sine
-	// Append two rows two matrix with the computed values at the respective positions
-	// matrix € IR^(2*#Gelenke x #stab)
-	var matrix       = new Matrix.fill(18,16,0);
-	var sortedStaebe = b.stab;//.sort(byLeftEnd);
-	for (var s = 0; s < sortedStaebe.length; s++) {
-		var current = sortedStaebe[s];
+	var mass      = 1; // FIXME: Read from list
+	var gravity   = 8.91; // FIXME: Read from site
+	var matrix    = new Matrix.fill(18,16,0);
+	var lengths   = new Array(b.stab.length);
+	var rightHand = new Matrix.fill(2,16,0);
+	var stab      = b.stab;
+	for (var s = 0; s < stab.length; s++) {
+		var current = stab[s];
 		matrix = insertIntoMatrix(s,current,matrix).copy();
-//		console.log(mat.lr());
 	}
-	console.log(matrix.replace(0, '    ').toString());
-	return matrix;
+	
+	for (var l = 0; l < b.stab.length; l++) {
+		lengths[l] = 1/b.stab[l].getDistance();
+	}
+	
+	for (var r = 0; r < rightHand.height; r++) {
+		rightHand.mtx[r][1] = mass * gravity;
+	}
+	console.log(matrix.replace(0, '     ').toString());
+	lengths = Matrix.diag(lengths);
+	return {
+		matrix: matrix,
+		lengths: lengths,
+		rightHand: rightHand
+	};
 };
-//	var a = [gelenk.getX(),gelenk.getY()];
-//	var s = a.spherical();
+
 function insertIntoMatrix(s,stab,matrix) {
 	var result = matrix.copy();
 	var left   = parseInt(stab.leftEnd);
 	var right  = parseInt(stab.rightEnd);
 	var leftG  = left > 1 ? b.gelenke[left-2] : undefined;
 	var rightG = right > 1 ? b.gelenke[right-2] : undefined;
-	if (leftG) {
-		var sp = [leftG.getX(), leftG.getY()];
-		sp = sp.spherical();
-		result.mtx[2*(left-2)][s] = sp.x.toFixed(2);
-		result.mtx[2*(left-2)+1][s] = sp.y.toFixed(2);
-	}
-	if (rightG) {
-		var sp = [rightG.getX(), rightG.getY()];
-		sp = sp.spherical();
-		result.mtx[2*(right-2)][s] = sp.x.toFixed(2);
-		result.mtx[2*(right-2)+1][s] = sp.y.toFixed(2);
+	if (leftG && rightG) {
+		var sp = [leftG.getX() - rightG.getX(), 
+			  leftG.getY() - rightG.getY()];
+		sp = sp.normalise();
+		result.mtx[2*(left-2)][s] = sp[0].toFixed(2);
+		result.mtx[2*(left-2)+1][s] = sp[1].toFixed(2);
+		sp = sp.mult(-1);
+		result.mtx[2*(right-2)][s] = sp[0].toFixed(2);
+		result.mtx[2*(right-2)+1][s] = sp[1].toFixed(2);
 	}
 	return result;
 }
 
 function solveLes() {
+	var preface = buildLes();
+	var matrix = preface.matrix;
+	var lengths   = preface.lengths;
+	var rightHand = preface.rightHand;
+	var result = matrix.mult(lengths).mult(matrix.transpose());
+	var part = result.lr();
+	var r = part.r;
+	var l = part.l;
+	y = l.forwardSubstitution(rightHand);
+	x = r.backwardSubstitution(y);
+	//console.log(result.mult(x).toString());
+	//paint in red and write to file
+	console.log(result.replace(0, '    ').toString());
+	console.log(result.mult(x).toString());
+	console.log(rightHand.toString());
+	return result;
 };
 
 function toArray(element) {
